3d2960938ba48b938a000a9e3b48b4343840f054
Avnish Kumar Sinha <93894240+avnishsinha@users.noreply.github.com>
2025-06-21 01:05:41 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 Mini-Project 2: API Integration
 Project Name: CS-312-MiniProject-2
 API: JokeAPI (returns a joke based on the user's name) Choosing this one because it was the most simple and easy to use.
-Tech Stack: Node.js, Express.js, Axios, EJS, HTML, CSS
 */
 
 const express = require("express");
